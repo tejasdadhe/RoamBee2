6538522a32c2eff91e91bc3f9529d90cfd5d7bfd
Tejas Dadhe <tejas.dekkodigital@gmail.com>
2019-07-05 11:50:12 +0530

    final commit

---
+++ scripts/script.js
@@ -0,0 +1,93 @@
+const area_of_requirement =['Shipment Monitoring','Inventory Monitoring','Returnable Asset Monitoring','Field Asset Monitoring','Others'];
+const mode_of_transport = ['Road','Rail','Ocean','Air'];
+var PostData = {
+    'blog_subscription':false,
+    'marketing_subscription':false,
+    'area_of_interest':'',
+    'mode_of_transport':'',
+    'first_name':'',
+    'last_name':'',
+    'email_id':'',
+    'contact_num':'',
+    'message':''
+};
+
+function getSelectedChbox() {
+    var selchbox = [];
+    var inpfields = document.getElementsByTagName('input');
+    var nr_inpfields = inpfields.length;
+    for(var i=0; i<nr_inpfields; i++) {
+      if(inpfields[i].type == 'checkbox' && inpfields[i].checked == true) selchbox.push(inpfields[i].value);
+    }
+    return selchbox;
+  } 
+
+function openXontactDetailsForm()
+{
+    if(document.querySelector('input[name="area_of_interest"]:checked') != null)
+    {
+        const form1 = document.getElementById('form-page-1');
+        const form2 = document.getElementById('form-page-2');
+
+        form1.style.display = 'none';
+        form2.style.display = 'block';
+
+        let i = document.querySelector('input[name="area_of_interest"]:checked').value;
+        PostData.area_of_interest = area_of_requirement[i];
+        PostData.mode_of_transport = getSelectedChbox();
+
+        console.log("PostData %j",PostData);
+    }
+}
+
+
+function sendData()
+{
+    PostData.first_name = document.getElementById('fname').value;
+    PostData.last_name = document.getElementById('lname').value;
+    PostData.email_id = document.getElementById('email').value;
+    PostData.contact_num = document.getElementById('contact_num').value;
+    PostData.message = document.getElementById('message').value;
+    PostData.blog_subscription = getCheckBoxValue('blog_sub');
+    PostData.marketing_subscription = getCheckBoxValue('marketing_sub');
+    
+    console.log("Post Data %j",PostData);
+    //post('./saveData.php', {'data': PostData});
+}
+
+function post(path, params, method='post') {
+    const form = document.createElement('form');
+    form.method = method;
+    form.action = path;
+  
+    for (const key in params) {
+      if (params.hasOwnProperty(key)) {
+        const hiddenField = document.createElement('input');
+        hiddenField.type = 'hidden';
+        hiddenField.name = key;
+        hiddenField.value = params[key];
+  
+        form.appendChild(hiddenField);
+      }
+    }
+  
+    document.body.appendChild(form);
+    form.submit();
+  }
+
+function showMenu()
+{
+    const form_container = document.getElementById("form-container");
+    form_container.style.display = "block";
+}
+
+function hideMenu()
+{
+    const form_container = document.getElementById("form-container");
+    form_container.style.display = "none";
+}
+
+function getCheckBoxValue(id)
+{
+   return document.getElementById(id).checked
+}
